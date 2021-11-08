4897d56d0bd98ad9cbdb21bc3e7e3a2721fa7b4e
AndresLupian <lupianro@gmail.com>
2021-11-08 14:37:49 -0600

    Asignando Valores

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,26 @@ const btnAgregar = document.querySelector('#btnAgregar');
 btnAgregar.addEventListener('click', ()=>{
     const divElemento = document.createElement('DIV');
     divElemento.classList.add('elemento')
+    
     const crearParrafo = document.createElement('P');
     crearParrafo.classList.add('txt');
+    crearParrafo.textContent = entrada.value;
+    
     const crearBotonEditar = document.createElement('Button');
     crearBotonEditar.classList.add('btnEditar');
+    crearBotonEditar.setAttribute('type', 'Button');
+    crearBotonEditar.textContent = "Editar";
+    
     const crearBotonBorrar = document.createElement('Button');
     crearBotonBorrar.classList.add('btnBorrar');
+    crearBotonBorrar.setAttribute('type', 'Button');
+    crearBotonBorrar.textContent = "Borrar";
+
 
     lienzo.appendChild(divElemento);
+    divElemento.appendChild(crearParrafo)
+    divElemento.appendChild(crearBotonEditar)
+    divElemento.appendChild(crearBotonBorrar)
+
     console.log(entrada.value)
 })
